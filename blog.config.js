349e0e1272a2ff16ca8fe5ec3ff39c4ce1b828b7
zhu-qingjun <73931044+zhu-qingjun@users.noreply.github.com>
2022-08-30 11:08:40 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -44,7 +44,7 @@ const BLOG = {
   notionAccessToken: process.env.NOTION_ACCESS_TOKEN, // Useful if you prefer not to make your database public
   telegramToken: process.env.TELEGRAM_TOKEN, // The token of your Telegram bot
   telegramChatId: '1067226153', // The chat id of your Telegram bot
-  telegramChannelUrl: 'https://t.me/+WHMBhqdglRo2Mjll', // The link of your Telegram channel
+  telegramChannelUrl: 'https://channel.zuolan.me/', // The link of your Telegram channel
   telegramChannelName: 'zhu_channel', // The name of your Telegram channel
   craftConfigShareUrl: 'https://www.craft.do/s/8gQSdBtbuPjpp1', // The link to share your craft config
   analytics: {
